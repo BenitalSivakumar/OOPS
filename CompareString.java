b38cdb535517d2f4166860b3f2ca80b5f653cec0
BenitalSivakumar <91796134+BenitalSivakumar@users.noreply.github.com>
2021-10-27 23:07:21 +0530

    Add files via upload

---
+++ CompareString.java
@@ -0,0 +1,30 @@
+package oopsAssignments;
+
+public class CompareString {
+	
+	public static void main(String[] args) {
+		String a="I am Learning Java" ;
+		String b="I am learning java";
+		if(a.equalsIgnoreCase(b))
+		{
+			System.out.println("equals");
+		}
+		else
+			System.out.println("not equal");
+				
+	
+	if(a==b)
+	{
+		System.out.println("equals");
+	}
+	else
+		System.out.println("not equal");
+			
+if(a.equals(b))
+{
+	System.out.println("equals");
+}
+else
+	System.out.println("not equal");
+}	
+}  
